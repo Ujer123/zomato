b8080aa230fee3a13ef61035761f21605f3f2f25
Ujer123 <ujermohd0@gmail.com>
2024-02-27 21:34:25 +0530

    zomato clone

---
+++ script.js
@@ -0,0 +1,10 @@
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    scale: 0.2,
+    delay: 200 
+});
+
+ScrollReveal().reveal('.animate-on-scroll', { origin: 'bottom' });
